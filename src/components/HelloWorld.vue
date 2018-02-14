6d068b7b8f31568ef7140f790f9b4ec8521b72d5
blueshark <blueshark@test.com>
2018-02-14 20:50:08 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,69 +1,118 @@
-<template>
-  <div class="main">
-     <h3>lishuai test page</h3>
-     <Button @click="instance('info')">Info</Button>
-     <Button @click="instance('success')">Success</Button>
-     <Button @click="instance('warning')">Warning</Button>
-     <Button @click="instance('error')">Error</Button>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
- methods: {
-     instance (type) {
-         const title = 'Title';
-         const content = '<p>Content of dialog</p><p>Content of dialog</p>';
-         switch (type) {
-             case 'info':
-                 this.$Modal.info({
-                     title: title,
-                     content: content
-                 });
-                 break;
-             case 'success':
-                 this.$Modal.success({
-                     title: title,
-                     content: content
-                 });
-                 break;
-             case 'warning':
-                 this.$Modal.warning({
-                     title: title,
-                     content: content
-                 });
-                 break;
-             case 'error':
-                 this.$Modal.error({
-                     title: title,
-                     content: content
-                 });
-                 break;
-         }
-     }
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .main{
-      text-align: center; /*让div内部文字居中*/
-      background-color: #fff;
-      border-radius: 20px;
-      width: 320px;
-      height: 350px;
-      margin: auto;
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-  }
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-header-bar{
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+    }
+    .menu-icon{
+        transition: all .3s;
+    }
+    .rotate-icon{
+        transform: rotate(-90deg);
+    }
+    .menu-item span{
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: width .2s ease .2s;
+    }
+    .menu-item i{
+        transform: translateX(0px);
+        transition: font-size .2s ease, transform .2s ease;
+        vertical-align: middle;
+        font-size: 16px;
+    }
+    .collapsed-menu span{
+        width: 0px;
+        transition: width .2s ease;
+    }
+    .collapsed-menu i{
+        transform: translateX(5px);
+        transition: font-size .2s ease .2s, transform .2s ease .2s;
+        vertical-align: middle;
+        font-size: 22px;
+    }
 </style>
+<template>
+    <div class="layout">
+        <Layout :style="{height: `${pageHeight}px`}">
+            <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+                <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+                    <MenuItem name="1-1">
+                        <Icon type="ios-navigate"></Icon>
+                        <span>Option 1</span>
+                    </MenuItem>
+                    <MenuItem name="1-2">
+                        <Icon type="search"></Icon>
+                        <span>Option 2</span>
+                    </MenuItem>
+                    <MenuItem name="1-3">
+                        <Icon type="settings"></Icon>
+                        <span>Option 3</span>
+                    </MenuItem>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Header :style="{padding: 0}" class="layout-header-bar">
+                    <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '20px 20px 0'}" type="navicon-round" size="24"></Icon>
+                </Header>
+                <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                isCollapsed: false,
+                pageHeight: 900
+            }
+        },
+        created() {
+          this.pageHeight = window.innerHeight - 2
+          window.onresize = () => {
+              return (() => {
+                  this.pageHeight = window.innerHeight - 2
+              })()
+          }
+        },
+        computed: {
+            rotateIcon () {
+                return [
+                    'menu-icon',
+                    this.isCollapsed ? 'rotate-icon' : ''
+                ];
+            },
+            menuitemClasses () {
+                return [
+                    'menu-item',
+                    this.isCollapsed ? 'collapsed-menu' : ''
+                ]
+            }
+        },
+        methods: {
+            collapsedSider () {
+                this.$refs.side1.toggleCollapse();
+            }
+        }
+    }
+</script>
