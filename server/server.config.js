68c728ba5680c3b2215f362f700fbf68d24b50fe
lishuai0214 <lishuai0214@gmail.com>
2023-12-23 18:24:57 +0800

    update

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 /**
  * Just replace BAREUR
  */ 
-const BAREURL = 'https://hnetweb.nbzdwss78.tk'
+const BAREURL = 'https://worker-frosty-resonance-d52a.blueshark.workers.dev'
 
 /**
  * Non-developers, please modify without authorization, 98% report errors!
